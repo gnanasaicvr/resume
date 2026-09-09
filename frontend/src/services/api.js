92645b8b8d3672a133c87ab84b9495f4e5e41d7d
Gnana Sai <gnanasaicvr@gmail@.com>
2026-09-09 14:53:08 +0530

    url changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://resume.onrender.com/api/candidates",
 });
 
 export const uploadResumes = async (files) => {
